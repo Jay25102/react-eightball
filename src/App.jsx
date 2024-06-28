a927125333f3ef626809d8ab07be4d8c709855b6
Jay25102 <jasonhuang25102@gmail.com>
2024-06-28 13:09:10 -0400

    eight ball

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import EightBall from "./EightBall";
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <EightBall msg="hello" color="red"/>
+        </div>
+    )
+}
+
+export default App;
